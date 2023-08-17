634ccddb35464e2c78f39cec00e5cfae06a82d73
Klodian Behrami <klodianbehrami@gmail.com>
2023-08-17 14:12:36 -0400

    bind

--- practice-for-ch-js-argument-exercises-long-practice-main/argument_exercises.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/argument_exercises.js
@@ -7,6 +7,27 @@ function sum() {
     return total;
 };
 
+let sum2 = function(num1, num2, ...otherNums) {
+    let total = num1 + num2;
+    let i = 0;
+    while (i < otherNums.length) {
+        total += otherNums[i];
+        i++
+    };
+    return total
+};
 
-
+let sum3 = function(num1, num2, ...otherNums) {
+    let total = num1 + num2;
+    otherNums.forEach(function(num) {
+        total += num;
+    });
+    return total;
+};
 console.log(sum(6,8,1,4))
+console.log(sum2(6,8,1,4))
+console.log(sum3(6,8,1,4))
+
+
+
+
